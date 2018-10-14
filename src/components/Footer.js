0227ac3dbde089b2a78aca590cf0725d86a1902b
Ashton <ashchristie@gmail.com>
2018-10-14 01:06:06 -0700

    updated footer to have state, broke out subcomponents

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styled from 'styled-components';
 import Link from 'gatsby-link';
 
@@ -34,7 +34,7 @@ const FooterWrapper = styled.footer`
   }
 `;
 
-const FooterLeft = styled.div`
+const FooterLeftWrapper = styled.div`
   flex: 100;
   line-height: 1.3rem;
   text-align: center;
@@ -48,7 +48,7 @@ const FooterLeft = styled.div`
   }
 `;
 
-const FooterCenter = styled.div`
+const FooterCenterWrapper = styled.div`
   flex: 1;
   display: none;
   @media (min-width: ${props => props.theme.widthTablet}) {
@@ -57,7 +57,7 @@ const FooterCenter = styled.div`
   }
 `;
 
-const FooterRight = styled.div`
+const FooterRightWrapper = styled.div`
   flex: 100;
   display: flex;
   justify-content: flex-end;
@@ -76,69 +76,107 @@ const FooterRight = styled.div`
   }
 `;
 
-const Footer = ({ pageScrolled, pageBottom, isHome }) => {
-  const showFooter = window.innerWidth >= 750 || pageBottom;
-  const showFooterLeft = !isHome || pageScrolled;
-  const showNavArrow = isHome && !pageScrolled;
-  const year = new Date().getFullYear();
+class Footer extends Component {
+  state = {};
 
-  return (
-    <FooterWrapper className={showFooter ? 'visible-fade' : 'hidden-fade'}>
-      <FooterLeft className={showFooterLeft ? 'visible' : 'hidden'}>
-        <p>© {year} Copyright Ashton Christie. All rights reserved.</p>
-        <p>
-          This site is built with{' '}
-          <a href="https://www.gatsbyjs.org/" target="_blank">
-            Gatsbyjs
-          </a>{' '}
-          and{' '}
-          <a href="https://www.contentful.com/" target="_blank">
-            Contentful
-          </a>
-          . The source code is hosted on{' '}
-          <a href="https://github.com/ashco/ashco-platform" target="_blank">
-            Github
-          </a>
-          .
-        </p>
-      </FooterLeft>
-      <FooterCenter className={showNavArrow ? 'visible' : 'hidden'}>
-        <NavArrow />
-      </FooterCenter>
-      <FooterRight>
-        <ul>
-          <li>
-            <a href="https://github.com/ashco" target="_blank">
-              <GithubIcon />
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://www.linkedin.com/in/ashtonchristie/"
-              target="_blank"
-            >
-              <LinkedInIcon />
-            </a>
-          </li>
-          <li>
-            <a href="https://twitter.com/AshCo_Io" target="_blank">
-              <TwitterIcon />
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://drive.google.com/file/d/14z2YxB2hESDel8_Ek3oySUdyQRvpTNF5/view"
-              target="_blank"
-            >
-              <ResumeIcon />
-            </a>
-          </li>
-        </ul>
-      </FooterRight>
-    </FooterWrapper>
-  );
+  componentDidUpdate() {
+    const { pageScrolled, pageBottom, isHome } = this.props;
 
-  // }
-};
+    const showFooter = window.innerWidth >= 750 || this.props.pageBottom;
+    const showFooterLeft = !isHome || pageScrolled;
+    const showNavArrow = isHome && !pageScrolled;
+
+    if (showFooter !== this.state.showFooter) {
+      this.setState({
+        showFooter,
+      });
+    }
+    if (showFooterLeft !== this.state.showFooterLeft) {
+      this.setState({
+        showFooterLeft,
+      });
+    }
+    if (showNavArrow !== this.state.showNavArrow) {
+      this.setState({
+        showNavArrow,
+      });
+    }
+  }
+
+  render() {
+    // const { pageScrolled, pageBottom, isHome } = this.props;
+    const { showFooter, showFooterLeft, showNavArrow } = this.state;
+
+    // const showFooter = window.innerWidth >= 750 || pageBottom;
+    // const showFooterLeft = !isHome || pageScrolled;
+    // const showNavArrow = isHome && !pageScrolled;
+    const year = new Date().getFullYear();
+
+    return (
+      <FooterWrapper className={showFooter ? 'visible-fade' : 'hidden-fade'}>
+        <FooterLeft showFooterLeft={showFooterLeft} year={year} />
+        <FooterCenter showNavArrow={showNavArrow} />
+        <FooterRight />
+      </FooterWrapper>
+    );
+  }
+}
+
+const FooterLeft = ({ showFooterLeft, year }) => (
+  <FooterLeftWrapper className={showFooterLeft ? 'visible' : 'hidden'}>
+    <p>© {year} Copyright Ashton Christie. All rights reserved.</p>
+    <p>
+      This site is built with{' '}
+      <a href="https://www.gatsbyjs.org/" target="_blank">
+        Gatsbyjs
+      </a>{' '}
+      and{' '}
+      <a href="https://www.contentful.com/" target="_blank">
+        Contentful
+      </a>
+      . The source code is hosted on{' '}
+      <a href="https://github.com/ashco/ashco-platform" target="_blank">
+        Github
+      </a>
+      .
+    </p>
+  </FooterLeftWrapper>
+);
+
+const FooterCenter = ({ showNavArrow }) => (
+  <FooterCenterWrapper className={showNavArrow ? 'visible' : 'hidden'}>
+    <NavArrow />
+  </FooterCenterWrapper>
+);
+
+const FooterRight = () => (
+  <FooterRightWrapper>
+    <ul>
+      <li>
+        <a href="https://github.com/ashco" target="_blank">
+          <GithubIcon />
+        </a>
+      </li>
+      <li>
+        <a href="https://www.linkedin.com/in/ashtonchristie/" target="_blank">
+          <LinkedInIcon />
+        </a>
+      </li>
+      <li>
+        <a href="https://twitter.com/AshCo_Io" target="_blank">
+          <TwitterIcon />
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://drive.google.com/file/d/14z2YxB2hESDel8_Ek3oySUdyQRvpTNF5/view"
+          target="_blank"
+        >
+          <ResumeIcon />
+        </a>
+      </li>
+    </ul>
+  </FooterRightWrapper>
+);
 
 export default Footer;
