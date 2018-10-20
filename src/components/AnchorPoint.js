97e5239fef2eb27706689ad34cd6a633737e2976
Ashton <ashchristie@gmail.com>
2018-10-20 15:35:37 -0700

    added anchor point comp for main navigation

---
+++ src/components/AnchorPoint.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const NavPoint = styled.div`
+  position: absolute;
+  /* top: ${props => (props.isMobile ? '-140px' : '-13vh')}; */
+  top: -13vh;
+`;
+
+export default NavPoint;
